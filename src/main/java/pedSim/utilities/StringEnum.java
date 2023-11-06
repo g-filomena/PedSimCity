c8a5a595640e3de0e3ab806935dce999d89a2da8
Gabriele Filomena <gfilo@liverpool.ac.uk>
2023-11-06 15:20:27 +0000

    Update StringEnum.java

--- src/main/java/pedSim/utilities/StringEnum.java
+++ src/main/java/pedSim/utilities/StringEnum.java
@@ -1,64 +1,34 @@
 package pedSim.utilities;
 
 public class StringEnum {
-	
-    public enum RouteChoice {
-        ROAD_DISTANCE, 
-        ANGULAR_CHANGE, 
-        TURNS, 
-        LANDMARKS_DISTANCE, 
-        LANDMARKS_ANGULAR, 
-        LOCAL_LANDMARKS_DISTANCE, 
-        LOCAL_LANDMARKS_ANGULAR,
-        DISTANT_LANDMARKS_DISTANCE, 
-        DISTANT_LANDMARKS_ANGULAR,
-        DISTANT_LANDMARKS,
-        REGION_DISTANCE,
-        REGION_BARRIER_DISTANCE,
-        REGION_ANGULAR,
-        REGION_BARRIER_ANGULAR,
-        REGION_TURNS,
-        BARRIER_DISTANCE,
-        BARRIER_ANGULAR,
-    }
-    
-    public enum RouteChoiceProperty {
-    	
-        ROAD_DISTANCE,
-        ANGULAR_CHANGE,
-        NR_TURNS,
-        ROAD_DISTANCE_LOCAL,
-        ANGULAR_CHANGE_LOCAL,
-        USING_ELEMENTS,
-        NOT_USING_ELEMENTS,
-        LOCAL_LANDMARKS,
-        BARRIER_SUBGOALS,
-        NO_SUBGOALS,
-        REGION_BASED,
-        NOT_REGION_BASED,
-        USING_DISTANT,
-        NOT_USING_DISTANT
-    }
-    
-    public enum LandmarkType {
-    	LOCAL, 
-    	GLOBAL
-    }
-    
-    
-    public enum BarrierType {
-    	ALL, 
-    	POSITIVE, 
-    	NEGATIVE,
-    	SEPARATING,
-    }
-    
-    public static String getAbbreviation(RouteChoice choice) {
-        String[] parts = choice.toString().split("_");
-        StringBuilder abbreviation = new StringBuilder();
-        for (String part : parts) {
-            abbreviation.append(part.charAt(0));
-        }
-        return abbreviation.toString();
-    }
+
+	public enum RouteChoice {
+		ROAD_DISTANCE, ANGULAR_CHANGE, LANDMARKS_DISTANCE, LANDMARKS_ANGULAR, LOCAL_LANDMARKS_DISTANCE,
+		LOCAL_LANDMARKS_ANGULAR, DISTANT_LANDMARKS_DISTANCE, DISTANT_LANDMARKS_ANGULAR, DISTANT_LANDMARKS,
+		REGION_DISTANCE, REGION_BARRIER_DISTANCE, REGION_ANGULAR, REGION_BARRIER_ANGULAR, BARRIER_DISTANCE,
+		BARRIER_ANGULAR,
+	}
+
+	public enum RouteChoiceProperty {
+
+		ROAD_DISTANCE, ANGULAR_CHANGE, ROAD_DISTANCE_LOCAL, ANGULAR_CHANGE_LOCAL, USING_ELEMENTS, NOT_USING_ELEMENTS,
+		LOCAL_LANDMARKS, BARRIER_SUBGOALS, NO_SUBGOALS, REGION_BASED, NOT_REGION_BASED, USING_DISTANT, NOT_USING_DISTANT
+	}
+
+	public enum LandmarkType {
+		LOCAL, GLOBAL
+	}
+
+	public enum BarrierType {
+		ALL, POSITIVE, NEGATIVE, SEPARATING,
+	}
+
+	public static String getAbbreviation(RouteChoice choice) {
+		String[] parts = choice.toString().split("_");
+		StringBuilder abbreviation = new StringBuilder();
+		for (String part : parts) {
+			abbreviation.append(part.charAt(0));
+		}
+		return abbreviation.toString();
+	}
 }
