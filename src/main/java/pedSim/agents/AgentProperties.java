8f3ed75cae522bc5e9ca0c51a6400116cadf66e5
Gabriele Filomena <gfilo@liverpool.ac.uk>
2023-11-07 15:18:40 +0000

    Update AgentProperties.java

--- src/main/java/pedSim/agents/AgentProperties.java
+++ src/main/java/pedSim/agents/AgentProperties.java
@@ -71,23 +71,13 @@ public class AgentProperties {
 			return;
 
 		// localHeuristics
-		localHeuristicDistance = containsDistance(routeChoice);
-		localHeuristicAngular = containsAngular(routeChoice);
+		localHeuristicDistance = containsDistance();
+		localHeuristicAngular = containsAngular();
 
 		// landmarks
-		regionBasedNavigation = containsRegion(routeChoice);
-		barrierBasedNavigation = containsBarrier(routeChoice);
-		usingLocalLandmarks = containsLocal(routeChoice);
-		usingDistantLandmarks = containsGlobal(routeChoice);
-
-		if (routeChoice.toString().contains("LANDMARKS")) {
-			usingDistantLandmarks = true;
-			if (!routeChoice.toString().contains("DISTANT"))
-				usingLocalLandmarks = true;
-		}
-
-		if (usingLocalLandmarks)
-			landmarkType = StringEnum.LandmarkType.LOCAL;
+		activateLandmarks();
+		regionBasedNavigation = containsRegion();
+		barrierBasedNavigation = containsBarrier();
 
 		if (barrierBasedNavigation) {
 			preferenceNaturalBarriers = true;
@@ -101,60 +91,90 @@ public class AgentProperties {
 	/**
 	 * Checks if the given route choice contains a "DISTANCE" component.
 	 *
-	 * @param routeChoice The route choice to check.
-	 * @return True if "DISTANCE" is found in the route choice, false otherwise.
+	 * @return True if "DISTANCE" is found in the route choice descriptor, false
+	 *         otherwise.
 	 */
-	public static boolean containsDistance(RouteChoice routeChoice) {
+	private boolean containsDistance() {
 		return routeChoice.toString().contains("DISTANCE");
 	}
 
 	/**
 	 * Checks if the given route choice contains an "ANGLE" component.
 	 *
-	 * @param routeChoice The route choice to check.
-	 * @return True if "ANGLE" is found in the route choice, false otherwise.
+	 * @return True if "ANGLE" is found in the route choice descriptor, false
+	 *         otherwise.
 	 */
-	public boolean containsAngular(RouteChoice routeChoice) {
+	private boolean containsAngular() {
 		return routeChoice.toString().contains("ANGLE");
 	}
 
 	/**
 	 * Checks if the given route choice contains a "REGION" component.
 	 *
-	 * @param routeChoice The route choice to check.
-	 * @return True if "REGION" is found in the route choice, false otherwise.
+	 * @return True if "REGION" is found in the route choice descriptor, false
+	 *         otherwise.
 	 */
-	public boolean containsRegion(RouteChoice routeChoice) {
+	private boolean containsRegion() {
 		return routeChoice.toString().contains("REGION");
 	}
 
 	/**
 	 * Checks if the given route choice contains a "BARRIER" component.
 	 *
-	 * @param routeChoice The route choice to check.
-	 * @return True if "BARRIER" is found in the route choice, false otherwise.
+	 * @return True if "BARRIER" is found in the route choice descriptor, false
+	 *         otherwise.
 	 */
-	public boolean containsBarrier(RouteChoice routeChoice) {
+	public boolean containsBarrier() {
 		return routeChoice.toString().contains("BARRIER");
+	}
+
+	/**
+	 * Activates the use of local and/or global landmarks based on the specified
+	 * conditions. Sets the values for usingLocalLandmarks and usingDistantLandmarks
+	 * based on the presence of local and global landmarks.
+	 */
+	private void activateLandmarks() {
+
+		usingLocalLandmarks = containsLocal();
+		usingDistantLandmarks = containsGlobal();
+
+		if (containsAllLandmarks()) {
+			usingDistantLandmarks = true;
+			if (!routeChoice.toString().contains("DISTANT"))
+				usingLocalLandmarks = true;
+		}
+
+		if (usingLocalLandmarks)
+			landmarkType = StringEnum.LandmarkType.LOCAL;
 	}
 
 	/**
 	 * Checks if the given route choice contains a "LOCAL" component.
 	 *
-	 * @param routeChoice The route choice to check.
-	 * @return True if "LOCAL" is found in the route choice, false otherwise.
+	 * @return True if "LOCAL" is found in the route choice descriptor, false
+	 *         otherwise.
 	 */
-	public boolean containsLocal(RouteChoice routeChoice) {
+	private boolean containsLocal() {
 		return routeChoice.toString().contains("LOCAL");
 	}
 
 	/**
 	 * Checks if the given route choice contains a "DISTANT" component.
 	 *
-	 * @param routeChoice The route choice to check.
-	 * @return True if "DISTANT" is found in the route choice, false otherwise.
+	 * @return True if "DISTANT" is found in the route choice descriptor, false
+	 *         otherwise.
 	 */
-	public boolean containsGlobal(RouteChoice routeChoice) {
+	private boolean containsGlobal() {
 		return routeChoice.toString().contains("DISTANT");
+	}
+
+	/**
+	 * Checks if the given route choice contains a "DISTANT" component.
+	 *
+	 * @return True if "DISTANT" is found in the route choice descriptor, false
+	 *         otherwise.
+	 */
+	private boolean containsAllLandmarks() {
+		return routeChoice.toString().contains("LANDMARKS");
 	}
 }
