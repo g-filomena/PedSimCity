3618a8d6d471f57e9ec19f57db3b3db2b1ca5c95
Gabriele Filomena <gfilo@liverpool.ac.uk>
2026-03-18 15:48:17 +0000

    Update StringEnum.java

--- src/main/java/pedsim/core/utilities/StringEnum.java
+++ src/main/java/pedsim/core/utilities/StringEnum.java
@@ -2,47 +2,60 @@ package pedsim.core.utilities;
 
 public class StringEnum {
 
-  public enum RouteChoiceProperty {
-    ROAD_DISTANCE, ANGULAR_CHANGE, ROAD_DISTANCE_LOCAL, ANGULAR_CHANGE_LOCAL, USING_ELEMENTS, NOT_USING_ELEMENTS, LOCAL_LANDMARKS, BARRIER_SUBGOALS, NO_SUBGOALS, REGION_BASED, NOT_REGION_BASED, USING_DISTANT, NOT_USING_DISTANT
-  }
+	public enum MinimisationMode {
+		NONE, DISTANCE, ANGULAR
+	}
 
-  public enum AgentStatus {
-    WALKING_ALONE, WAITING, GOING_HOME, AT_DESTINATION,
-  }
+	public enum LocalHeuristicMode {
+		NONE, DISTANCE, ANGULAR
+	}
 
-  public enum LandmarkNavigationMode {
-    CITY_LEVEL, REGION_BASED
-  }
+	public enum RouteChoiceElement {
+		LOCAL_LANDMARKS, DISTANT_LANDMARKS, REGION_BASED_NAVIGATION, BARRIER_BASED_NAVIGATION
+	}
 
-  public enum RouteMeaningfulnessFactor {
-    EASINESS, EXPOSURE, NOVELTY
-  }
+	public enum RouteChoiceProperty {
+		ROAD_DISTANCE, ANGULAR_CHANGE, ROAD_DISTANCE_LOCAL, ANGULAR_CHANGE_LOCAL, USING_ELEMENTS, NOT_USING_ELEMENTS,
+		LOCAL_LANDMARKS, BARRIER_SUBGOALS, NO_SUBGOALS, REGION_BASED, NOT_REGION_BASED, USING_DISTANT, NOT_USING_DISTANT
+	}
 
-  public enum Learner {
-    LEARNER, NOT_LEARNER
-  }
+	public enum AgentStatus {
+		WALKING_ALONE, WAITING, GOING_HOME, AT_DESTINATION,
+	}
 
-  public enum LandmarkType {
-    LOCAL, GLOBAL
-  }
+	public enum LandmarkNavigationMode {
+		CITY_LEVEL, REGION_BASED
+	}
 
-  public enum AgentBarrierType {
-    ALL, POSITIVE, NEGATIVE, SEPARATING
-  }
+	public enum RouteMeaningfulnessFactor {
+		EASINESS, EXPOSURE, NOVELTY
+	}
 
-  public enum BarrierType {
-    PARK, WATER, ROAD, RAILWAY, SECONDARY_ROAD
-  }
+	public enum Learner {
+		LEARNER, NOT_LEARNER
+	}
 
-  public enum RoadType {
-    PRIMARY, SECONDARY, TERTIARY, NEIGHBOURHOOD, UNKNOWN
-  }
+	public enum LandmarkType {
+		LOCAL, GLOBAL
+	}
 
-  public enum Vulnerable {
-    VULNERABLE, NON_VULNERABLE
-  }
+	public enum AgentBarrierType {
+		ALL, POSITIVE, NEGATIVE, SEPARATING
+	}
 
-  public enum TimeOfDay {
-    DAY, NIGHT
-  }
+	public enum BarrierType {
+		PARK, WATER, ROAD, RAILWAY, SECONDARY_ROAD
+	}
+
+	public enum RoadType {
+		PRIMARY, SECONDARY, TERTIARY, NEIGHBOURHOOD, UNKNOWN
+	}
+
+	public enum Vulnerable {
+		VULNERABLE, NON_VULNERABLE
+	}
+
+	public enum TimeOfDay {
+		DAY, NIGHT
+	}
 }
