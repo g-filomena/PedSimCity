08d8cd6b404a84c89df0d78e3fa8c58f7a5fc85e
Akos Balog <sgabalog@liverpool.ac.uk>
2026-06-29 08:39:04 +0100

    Organise data files: move older files to Torino_simplified and clean up root Torino folder

---
+++ TestSelect.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class TestSelect {
+    public static void main(String[] args) {
+        int agentsSize = 20000;
+        int nrAgents = 19999;
+        Random random = new Random();
+        Set<Integer> selectedAgents = new HashSet<>();
+        long start = System.currentTimeMillis();
+        long iter = 0;
+        while (selectedAgents.size() < Math.min(nrAgents, agentsSize)) {
+            int weightedIndex = (int) (Math.pow(random.nextDouble(), 1.5) * agentsSize);
+            selectedAgents.add(weightedIndex);
+            iter++;
+        }
+        System.out.println("Done in " + (System.currentTimeMillis() - start) + "ms. Iters: " + iter);
+    }
+}
